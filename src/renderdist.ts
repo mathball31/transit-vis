8f62de8a0bca73b1de3686c623d7743870c3319f
Dirk Lamb <dirk.lamb31@gmail.com>
2025-10-14 23:04:13 -0600

    angular setup

--- src/renderdist.ts
+++ src/renderdist.ts
@@ -1,18 +1,24 @@
 import { type Distribution } from "./distribution.js"
 
 
-export function renderDistribution(ctx: CanvasRenderingContext2D, dist: Distribution, dx: number, dy: number) {
+export function renderDistribution(
+    ctx: CanvasRenderingContext2D, 
+    dist: Distribution, 
+    dx: number, 
+    dy: number, 
+    color = {red: 1, green: 1, blue: 1}
+) {
     const imageData = ctx.createImageData(dist.width, dist.height)
     const data = imageData.data
     console.log(dist.data)
     for (let i = 0; i * 4 < data.length; i++) {
-        data[4*i] = (dist.data[i] ?? 0) * 255       //red
-        data[4*i+1] = (dist.data[i] ?? 0) * 255     // green
-        data[4*i+2] = (dist.data[i] ?? 0) * 255     // blue
-        data[4*i+3] = 1 * 255     // transparency
+        data[4*i] = 255-(((dist.data[i] ?? 0)  * 255) * (1- color.red))
+        data[4*i+1] = 255-(((dist.data[i] ?? 0)  * 255) * (1-color.green))
+        data[4*i+2] = 255-(((dist.data[i] ?? 0)  * 255) * (1-color.blue))
+        data[4*i+3] = .5 * 255     // transparency
     }
 
-    console.log(data)
+    // console.log(data)
 
     ctx.putImageData(imageData, dx, dy)
 
